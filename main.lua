f33b62c67ce94bba35211c25dd8af23e575f7bb0
Pete Hunter <pete.hunter@piksel.com>
2019-03-12 01:04:59 +0000

    removed clone.

--- main.lua
+++ main.lua
@@ -113,7 +113,7 @@ local function getManifest(tab, path, folder)
           getManifest(tab, path, file)
         end
       else
-        table.insert(filePath.."/"..file)
+        table.insert(tab, filePath.."/"..file)
       end
     end
   end
