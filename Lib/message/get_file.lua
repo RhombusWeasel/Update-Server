4587a0b7a26eafa11ebe4f1d027e8bec58fc5943
Pete Hunter <pete.hunter@piksel.com>
2019-03-13 02:38:22 +0000

    Added file pusher.

--- Lib/message/get_file.lua
+++ Lib/message/get_file.lua
@@ -3,11 +3,16 @@ return function(event, pkt)
   local path = pkt.path
   local ext = string.sub(path, #path - 3, #path)
   if ext == ".lua" or ext == ".sha" then
-    local f = io.open("./"..path, "rb")
+    local f = io.open("./"..path, "r")
+    local s_data = f:read("*a")
+    local b_data = {}
+    for i = 1, #s_data do
+      b_data[i] = string.byte(str, i).." "
+    end
     local pkt = {
       command = "write_file",
       path = path,
-      data = f:read("*a")
+      data = b_data,
     }
     f:close()
     event.peer:send(engine.string.serialize(pkt))
